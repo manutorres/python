79aff228609f17235d1a18fc6e6dee5cad8b705a
Manu Torres <jmtorresluc@gmail.com>
2022-09-21 17:24:43 -0300

    For loops. Hasta clase 65

--- 1. Strings, operators, replace fields/strings2.py
+++ 1. Strings, operators, replace fields/strings2.py
@@ -30,11 +30,27 @@ print()
 
 print(parrot[0:6:2])    # Nre
 print(parrot[0:6:3])    # Nw
+print()
+print("-" * 80)
 
-number = "9,223;342:234 456,982;234"
-separators = number[1::4]
+#number = "9,223;342:234 456,982;234"
+#separators = number[1::4]   #Slice de posicion 1 al final saltando de a 4
+
+number = input("Please enter a series of numbers, using any separators you like: ")
+# Si no supiera la posicion de los separadores
+separators = ""
+for char in number:
+    if not char.isnumeric():
+        separators += char
+
 print(separators)
 print()
 
-values = "".join(char if char not in separators else " " for char in number).split()
-print([int(val) for val in values])
+values = "".join(char if char not in separators else " " for char in number)
+print("Separadores reemplazados por espacios: " + values)
+split_values = values.split()
+print("Valores separados en una lista de strings: {}".format(split_values))
+int_values = [int(val) for val in split_values]
+print("Valores convertidos a una lista de int: {}".format(int_values))
+
+print("La suma total: {}".format(sum(int_values)))
